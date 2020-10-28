97f53ed987e0420b88c03818168a37907fe60f56
Navid <torabazary@yahoo.com>
2020-10-28 23:50:11 +0200

    Opencart V2.0.x up to V2.2.x

---
+++ v.2.0.x - 2.2.x/catalog/model/payment/mobilpay.php
@@ -0,0 +1,32 @@
+<?php 
+class ModelPaymentMobilpay extends Model {
+  	public function getMethod($address, $total) {
+		$this->load->language('payment/mobilpay');
+		
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('mobilpay_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+		
+		if ($this->config->get('mobilpay_total') > $total) {
+			$status = false;
+		} else if (!$this->config->get('mobilpay_geo_zone_id')) {
+			$status = true;
+		} elseif ($query->num_rows) {
+			$status = true;
+		} else {
+			$status = false;
+		}	
+		
+		$method_data = array();
+	
+		if ($status) {  
+      		$method_data = array( 
+        		'code'       => 'mobilpay',
+        		'title'      => $this->language->get('text_title'),
+			'terms'      => '',
+			'sort_order' => $this->config->get('mobilpay_sort_order')
+      		);
+    	}
+   
+    	return $method_data;
+  	}
+}
+?>
